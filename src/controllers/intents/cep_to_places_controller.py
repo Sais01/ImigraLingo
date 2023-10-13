0c102beacbafea813071ed29b5b39cdf1a0d36c7
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-10-13 11:27:16 -0300

    :sparkles: feat: Adicionando parametros à função handle_cep_to_places e atualizando requirements.

--- src/controllers/intents/cep_to_places_controller.py
+++ src/controllers/intents/cep_to_places_controller.py
@@ -1,11 +1,16 @@
 from services.geo_service import cep_to_location, location_to_coordinates, places_by_coordinates
+from utils import prepare_response_text
 
-def handle_cep_to_places(cep):
+def handle_cep_to_places(event, context):
+  slots = event['interpretations'][0]['intent']['slots']
+  cep = slots['cep']['value']['interpretedValue']
   address = cep_to_location(cep)
   if address:
     lat, lon = location_to_coordinates(address)
-    places_by_coordinates(lat, lon)
+    response = places_by_coordinates(lat, lon)
+
+    return prepare_response_text(event, response)
 
 # TESTE
-if __name__ == "__main__":
-  handle_cep_to_places("96203410")
+# if __name__ == "__main__":
+#   handle_cep_to_places("96203410")
