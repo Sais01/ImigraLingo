8f9fa48fdcb6f178bebd30b99a9c12b96333b2fd
angemydelson <delsonysuperdel@gmail.com>
2023-10-16 17:16:07 -0300

    :sparkles: feat: Adicionando função para traduzir Texto pt-BR para texto em FR e Texto FR para texto pt-BR

--- src/services/translate_service.py
+++ src/services/translate_service.py
@@ -1,2 +1,17 @@
+import boto3
+
 def text_translate(text, source_language, target_language):
-  pass
+    # Configure o cliente do Amazon Translate
+    translate_client = boto3.client('translate', region_name='us-east-1')
+
+    # Realize a tradução
+    result = translate_client.translate_text(
+        Text=text,
+        SourceLanguageCode=source_language,
+        TargetLanguageCode=target_language
+    )
+
+    # Extraia o texto traduzido do resultado
+    translated_text = result['TranslatedText']
+
+    return translated_text
