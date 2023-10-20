9b88dd9e18109a602ec4164dc8321f0ef258a1be
Matheus Pinto <matheuszsp@gmail.com>
2023-10-20 10:49:56 -0300

    ♻️refact: movendo rekognition para services

--- src/utils/decode_response_data.py
+++ src/utils/decode_response_data.py
@@ -1,7 +1,18 @@
 import json
-def decode_response_data(response_data):
-    json_data = json.dumps(response_data)
-    decoded_data = json_data.encode("utf-8").decode("unicode-escape")
-    decoded_data = decoded_data.replace('"', "'")
 
-    return decoded_data
+
+def decode_response_data(phrase):
+
+    try:
+        # json_data = json.dumps(response_data)
+        json_data = json.dumps({"phrase": phrase}, indent=4)
+        decoded_data = json_data.encode("utf-8").decode("unicode-escape")
+
+        # json_object = json.loads(json_string)
+
+
+        return {"statusCode": 200, "body": decoded_data}
+
+    except Exception as e:
+        print(e)
+        return {"statusCode": 404, "body": "ERROR"}
