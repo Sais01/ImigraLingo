cabeca09a703b8b0fc163e22bcfcacd519de254b
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-10-23 15:29:06 -0300

    :sparkles: feat: Finalizando retornos de texto na intent de imagem.

--- src/controllers/intents/image_text_extraction_controller.py
+++ src/controllers/intents/image_text_extraction_controller.py
@@ -1,15 +1,31 @@
 from core.config import settings
 from services.rekognition_service import extract_text_from_image
 from utils.response_formatters import prepare_response_text
+from services.translate_service import text_translate
 
 def handle_image_to_text(event, context):
     try:
         BUCKET_NAME = settings.BUCKET_NAME
-        IMAGE_NAME = settings.IMAGE_NAME
         
-        response_text_from_image = extract_text_from_image(BUCKET_NAME, IMAGE_NAME)
+        slots = event['interpretations'][0]['intent']['slots']
+        image_name = slots['imgFromUser']['value']['originalValue']
+        text_or_audio_conditional = slots['textOrAudioConditional']['value']['originalValue']
+
+        response_text_from_image = extract_text_from_image(BUCKET_NAME, image_name)
 
         text_from_image = response_text_from_image["body"]["phrase"]
+
+        if text_or_audio_conditional == "text_fr":
+            text_from_image = text_translate(text_from_image, "pt", "fr")
+            return prepare_response_text(event, text_from_image)
+        
+        elif text_or_audio_conditional == "audio_pt":
+            return prepare_response_text(event, text_from_image)
+        
+        elif text_or_audio_conditional == "audio_fr":
+            return prepare_response_text(event, text_from_image)
+        else:
+            return prepare_response_text(event, text_from_image)
 
         print("1######################")
         print(response_text_from_image["body"])
@@ -19,12 +35,7 @@ def handle_image_to_text(event, context):
         print(text_from_image)
         print("2@@@@@@@@@@@@@@@@@@@@@@")
 
-
-        # slots = event["interpretations"][0]["intent"]["slots"]
-
-        return prepare_response_text(event, text_from_image)
-
-
+        
 
     except Exception as e:
         print(f"Error in image_text_extraction_controller: {e}")
