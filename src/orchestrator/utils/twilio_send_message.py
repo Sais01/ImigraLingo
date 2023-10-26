60e95e509c7574fe88d0304a0653785af90508d1
Matheus Pinto <matheuszsp@gmail.com>
2023-10-26 13:47:53 -0300

    ⚒️fix: Reorganizando o orchestrator e add env - Foi separado um arquivo para a função de envio de mensagens - Foram organizados as env var do orchestrator

---
+++ src/orchestrator/utils/twilio_send_message.py
@@ -0,0 +1,33 @@
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+from core.config import settings
+
+import boto3
+
+# Initialize the S3 client
+s3 = boto3.client('s3')
+
+TWILIO_WHATS_FROM = settings.TWILIO_WHATS_FROM
+TWILIO_WHATS_TO = settings.TWILIO_WHATS_TO
+TWILIO_ID = settings.TWILIO_ID
+TWILIO_TOKEN = settings.TWILIO_TOKEN
+
+def twilio_send_message(text_message):
+
+    client = Client(TWILIO_ID, TWILIO_TOKEN)
+
+    message = client.messages.create(
+        from_=TWILIO_WHATS_FROM,
+        body=text_message,
+        to=TWILIO_WHATS_TO,
+    )
+
+    
+    # If it's a text message, you can respond accordingly
+    response = MessagingResponse()
+    response.message("Received your text message. Thank you!")
+
+    return {
+        "statusCode": 200,
+        "body": str(response),
+    }
