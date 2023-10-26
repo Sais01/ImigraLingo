31b79b8b63beced4999e95caf4f1555cba16d72f
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-10-26 10:19:54 -0300

    :recycle: refac: Refatorando código da controller TextAudioTranslater.

--- src/lex/controllers/intents/text_audio_translater_controller.py
+++ src/lex/controllers/intents/text_audio_translater_controller.py
@@ -3,8 +3,20 @@ from services.polly_service      import text_converter, text_converted_s3_upload
 from services.translate_service  import text_translate
 from services.transcribe_service import audio_to_text
 from utils.response_formatters   import prepare_response_text
+import uuid
 
 def handle_text_audio_translater(event, context):
+  """
+  Handles the text/audio translation request by extracting the necessary information from the event object,
+  translating the text/audio to the desired language, and returning the translated text/audio as a response.
+
+  Args:
+    event (dict): The event object containing the user input and other relevant information.
+    context (dict): The context object containing the runtime information.
+
+  Returns:
+    dict: The response object containing the translated text/audio.
+  """
   try:
     bucket_name = settings.BUCKET_NAME
 
@@ -15,41 +27,31 @@ def handle_text_audio_translater(event, context):
     user_text_or_audio_receiver    = slots["textOrAudioReceiver"]["value"]["originalValue"]
     user_text_or_audio_conditional = slots["textOrAudioConditional"]["value"]["originalValue"]
 
-    if (user_language_conditional == "ptToFr"):
-      language = "pt-BR"
-      if (text_or_audio_user_input == "audio"):
-        text_to_translate = audio_to_text(user_text_or_audio_receiver, language, bucket_name)
-      else:
-        text_to_translate = user_text_or_audio_receiver
+ 
+    if user_language_conditional == "ptToFr":
+      from_lang_translate_and_polly = "pt"
+      to_lang_translate_and_polly   = "fr"
+      language_audio_transcribe = "pt-BR"
     else:
-      language = "fr-FR"
-      if (text_or_audio_user_input == "audio"):
-        text_to_translate = audio_to_text(user_text_or_audio_receiver, language, bucket_name)
-      else:
-        text_to_translate = user_text_or_audio_receiver
+      from_lang_translate_and_polly = "fr"
+      to_lang_translate_and_polly = "pt"
+      language_audio_transcribe = "fr-FR"
+
+    
+    if (text_or_audio_user_input == "audio"):
+      text_to_translate = audio_to_text(user_text_or_audio_receiver, language_audio_transcribe, bucket_name)
+    else:
+      text_to_translate = user_text_or_audio_receiver
 
 
-    if (user_language_conditional == "ptToFr"):
-      text_or_audio_translated = text_translate(text_to_translate, "pt", "fr")
+    text_or_audio_translated = text_translate(text_to_translate, from_lang_translate_and_polly, to_lang_translate_and_polly)
 
-      if (user_text_or_audio_conditional == "text"):
-        return prepare_response_text(event, text_or_audio_translated)
-      
-      if (user_text_or_audio_conditional == "audio"):
-        audio_response = text_converter(text_or_audio_translated, "fr")
-        audio_s3_response = text_converted_s3_upload(audio_response, bucket_name, "testeAudioTranslate")
-        return prepare_response_text(event, audio_s3_response)
-
-    elif (user_language_conditional == "frToPt"):
-      text_or_audio_translated = text_translate(text_to_translate, "fr", "pt")
-
-      if (user_text_or_audio_conditional == "text"):
-        return prepare_response_text(event, text_or_audio_translated)
-        
-      elif (user_text_or_audio_conditional == "audio"):
-        audio_response = text_converter(text_or_audio_translated, "pt")
-        audio_s3_response = text_converted_s3_upload(audio_response, bucket_name, "testeAudioTranslate")
-        return prepare_response_text(event, audio_s3_response)
+    if (user_text_or_audio_conditional == "text"):
+      return prepare_response_text(event, text_or_audio_translated)
+    else:
+      audio_response = text_converter(text_or_audio_translated, to_lang_translate_and_polly)
+      audio_s3_response = text_converted_s3_upload(audio_response, bucket_name, str(uuid.uuid4()))
+      return prepare_response_text(event, audio_s3_response)
         
   except Exception as e:
     print(f"Error in image_text_extraction_controller: {e}")
