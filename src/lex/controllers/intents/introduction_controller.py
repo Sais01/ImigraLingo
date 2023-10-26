195065fc8431003ef8ab379c443090f05027e0c3
Crisofer Gaier Sais <cristofer.gaier.sais@gmail.com>
2023-10-26 11:45:17 -0300

    :sparkles: feat: Redefinindo router_controllers para trabalhar com variáveis de ambiente e alterando seu local para a pasta controllers.

---
+++ src/lex/controllers/intents/introduction_controller.py
@@ -0,0 +1,15 @@
+from utils.response_formatters import prepare_response_text
+
+def handle_introduction(event, context):
+    """
+    Handles the introduction intent, which welcomes the user to the ImigraLingo bot and provides instructions on how to use it.
+
+    Args:
+        event: The event data passed by Amazon Lex.
+        context: The context data passed by Amazon Lex.
+
+    Returns:
+        A response object containing the welcome message and instructions.
+    """
+    response = f"Boas vindas ao ImigraLingo, bot de ajuda ao imigrante! O que deseja fazer? Caso não conheça meus comandos, digite 'ajuda'!"
+    return prepare_response_text(event, response)
---
+++ src/lex/controllers/router_controller.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+import os
+
+# reuse client connection as global
+client = boto3.client('lambda')
+
+def handle_lex_intent_verifier(event, context):
+    intent_name = event['sessionState']['intent']['name']
+    fn_name = os.environ.get(intent_name)
+    print(f"Intent: {intent_name} -> Lambda: {fn_name}")
+    if (fn_name):
+        # invoke lambda and return result
+        invoke_response = client.invoke(FunctionName=fn_name, Payload = json.dumps(event))
+        print(invoke_response)
+        payload = json.load(invoke_response['Payload'])
+        return payload
+    raise Exception('No environment variable for intent: ' + intent_name)
